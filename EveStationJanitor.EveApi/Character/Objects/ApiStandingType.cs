e41fdc2c5d996eb45981ec3f1a4cd0e18daa2112
Ionis <careless@astrocoup.com>
2024-11-01 11:02:54 +0000

    Use correct formatting for ApiStandingType enum value names and add a JsonConverter to map between string values returned by API and these changed enum values

--- EveStationJanitor.EveApi/Character/Objects/ApiStandingType.cs
+++ EveStationJanitor.EveApi/Character/Objects/ApiStandingType.cs
@@ -1,11 +1,39 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EveStationJanitor.EveApi.Character.Objects;
 
-[JsonConverter(typeof(JsonStringEnumConverter<ApiStandingType>))]
+[JsonConverter(typeof(ApiStandingTypeConverter))]
 public enum ApiStandingType
 {
-    agent,
-    npc_corp,
-    faction
+    Agent,
+    NpcCorp,
+    Faction
+}
+
+internal class ApiStandingTypeConverter : JsonConverter<ApiStandingType>
+{
+    public override ApiStandingType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return value switch
+        {
+            "agent" => ApiStandingType.Agent,
+            "npc_corp" => ApiStandingType.NpcCorp,
+            "faction" => ApiStandingType.Faction,
+            _ => throw new JsonException($"Value '{value}' is not valid for ApiStandingType")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, ApiStandingType value, JsonSerializerOptions options)
+    {
+        var stringValue = value switch
+        {
+            ApiStandingType.Agent => "agent",
+            ApiStandingType.NpcCorp => "npc_corp",
+            ApiStandingType.Faction => "faction",
+            _ => throw new JsonException($"Value '{value}' is not valid for ApiStandingType")
+        };
+        writer.WriteStringValue(stringValue);
+    }
 }
