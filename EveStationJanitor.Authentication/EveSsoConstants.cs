b116ae2cc64f86b6e6dd0bf3af083a634597de4b
Ionis <careless@astrocoup.com>
2024-11-14 13:00:24 +0000

    Convert EveSsoConstants to use file-scoped namespace

--- EveStationJanitor.Authentication/EveSsoConstants.cs
+++ EveStationJanitor.Authentication/EveSsoConstants.cs
@@ -1,9 +1,8 @@
-﻿namespace EveStationJanitor.Authentication
+﻿namespace EveStationJanitor.Authentication;
+
+public static class EveSsoConstants
 {
-    public static class EveSsoConstants
-    {
-        public const string TokenEndpoint = "https://login.eveonline.com/v2/oauth/token";
-        public const string AuthorizeEndpoint = "https://login.eveonline.com/v2/oauth/authorize";
-        public const string JsonWebKeySetsEndpoint = "https://login.eveonline.com/oauth/jwks";
-    }
+    public const string TokenEndpoint = "https://login.eveonline.com/v2/oauth/token";
+    public const string AuthorizeEndpoint = "https://login.eveonline.com/v2/oauth/authorize";
+    public const string JsonWebKeySetsEndpoint = "https://login.eveonline.com/oauth/jwks";
 }
